7a5ad8a59f8cd69b5e729cd733448349fc99ae58
calebmpeterson <caleb.m.peterson@gmail.com>
2014-04-10 07:00:47 -0500

    added auto-tail for .log files

--- init.el
+++ init.el
@@ -1,6 +1,12 @@
 (add-to-list 'load-path "~/.emacs.d/better-defaults")
 (load-library "better-defaults")
 
+;; Automatically enter auto-revert-tail-mode
+(add-to-list 'auto-mode-alist '("\\.out\\'" . auto-revert-mode))
+;; Automatically set coding system for logs
+(modify-coding-system-alist 'file "\\.out\\'" 'dos)
+
+
 (add-to-list 'load-path "~/.emacs.d/paredit")
 (load-library "paredit")
 
@@ -18,3 +24,18 @@
 
 (add-to-list 'load-path "~/.emacs.d/cider")
 (require 'cider)
+
+(global-linum-mode 1)
+(setq linum-format "%3d ")
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(linum ((t (:inherit (default default))))))
